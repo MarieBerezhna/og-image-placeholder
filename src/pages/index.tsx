2460caf94b064fac5d6c14af13c8cf319f332224
Marie Berezhna <marie.berezhna@gmail.com>
2025-03-06 14:50:22 +0100

    better input handling + enter key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,13 @@ import dynamic from "next/dynamic";
 const Button = dynamic(() => import('../components/ui/Button'), { ssr: false });
 const TextInput = dynamic(() => import('../components/ui/Input'), { ssr: false });
 
+type Event = { key: string; preventDefault: () => void; target: { value: SetStateAction<string>; }};
 export default function Home() {
   const [title, setTitle] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   const generateImage = () => {
+    setImageUrl(null);
     const encodedTitle = encodeURIComponent(title);
     setImageUrl(`/api/og?title=${encodedTitle}`);
   };
@@ -19,14 +21,17 @@ export default function Home() {
   return (
     <Flex style={{ gap: "1rem" }}>
       <h1>OpenGraph Image Generator</h1>
-
+      <Flex style={{ flexDirection: "row"}}>
       <TextInput
         placeholder="Enter a title..."
         value={title}
-        onChange={(e: { target: { value: SetStateAction<string>; }; }) => setTitle(e.target.value)}
+        onChange={(e: Event) => setTitle(e.target.value)}
+        onKeyDown={(e: Event) => e.key === "Enter" && generateImage()}
       />
 
-      <Button onClick={generateImage}>Generate Image</Button>
+      <Button onClick={generateImage}>Go!</Button>
+      </Flex>
+
       {imageUrl && <Preview imageUrl={imageUrl} />}
     </Flex>
   );
