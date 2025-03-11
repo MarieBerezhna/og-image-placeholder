0006026b86852b9b439c2376dfa273a3f4bceca8
Marie Berezhna <marie.berezhna@gmail.com>
2025-03-11 16:01:39 +0100

    use checkmark on copy

--- src/components/ClipboardLink.tsx
+++ src/components/ClipboardLink.tsx
@@ -3,8 +3,9 @@ import Button from "./ui/Button";
 import Flex from "./ui/Flex";
 
 export default function ClipboardLink({ href }: { href: string }) {
+	const defaultText = "Copy";
 	const [title, setTitle] = useState("Copy to clipboard");
-	const [text, setText] = useState("Copy");
+	const [text, setText] = useState(defaultText);
 	const onCopy = () => {
 		navigator.clipboard.writeText(href);
 		setTitle("Copied to clipboard");
@@ -24,6 +25,7 @@ export default function ClipboardLink({ href }: { href: string }) {
 				<code>{href}</code>
 			</pre>
 			<Button onClick={onCopy} title={title}>
+				{text !== defaultText && <span>&#10003;</span>}
 				{text}
 			</Button>
 		</Flex>
